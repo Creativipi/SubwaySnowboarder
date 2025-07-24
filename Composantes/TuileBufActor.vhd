fed8a5aecb4f8ae5c0b57c5581ad5c1263c533b2
xaviergs27 <60618027+xaviergs27@users.noreply.github.com>
2025-07-24 15:42:00 -0400

    Update TuileBufActor

--- Composantes/TuileBufActor.vhd
+++ Composantes/TuileBufActor.vhd
@@ -1,48 +1,53 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 08.06.2025 00:00:57
--- Design Name: 
--- Module Name: TuileBufActor - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity TuileBufActor is
-    Port ( i_tile_id : in STD_LOGIC_VECTOR (3 downto 0);
-           i_flip_x : in STD_LOGIC;
-           i_flip_y : in STD_LOGIC;
-           i_pix_x : in STD_LOGIC_VECTOR (3 downto 0);
-           i_pix_y : in STD_LOGIC_VECTOR (3 downto 0);
-           o_color_code : out STD_LOGIC_VECTOR (3 downto 0));
+    Port(
+    i_x : in STD_LOGIC_VECTOR (3 downto 0);
+    i_y : in STD_LOGIC_VECTOR (3 downto 0);
+    i_tile_id : in STD_LOGIC_VECTOR (3 downto 0);
+    i_ch_x : in STD_LOGIC_VECTOR (3 downto 0);
+    i_ch_y : in STD_LOGIC_VECTOR (3 downto 0);
+    i_ch_cc : in STD_LOGIC_VECTOR (4 downto 0);
+    i_ch_we : in std_logic;
+    i_clk : in STD_LOGIC;
+    i_flip_y : in STD_LOGIC;
+    o_colorCode : out STD_LOGIC_VECTOR (3 downto 0));
 end TuileBufActor;
 
 architecture Behavioral of TuileBufActor is
-
+    type tuile_out_array_t is array (0 to 7) of std_logic_vector(3 downto 0);
+    signal tuile_outputs : tuile_out_array_t;
+    signal tuile_write_enable : std_logic_vector(7 downto 0);
 begin
+    
+    gen_tuile_instances : for i in 0 to 7 generate
+        tuile_inst : entity work.Tuile_16x16
+            generic map (
+                TILE_ID => i
+            )
+            port map (
+                i_x => i_x,
+                i_y => i_y,
+                i_clk => i_clk,
+                i_ch_x => i_ch_x,
+                i_ch_y => i_ch_y,
+                i_ch_cc => i_ch_cc,
+                i_ch_we => tuile_write_enable(i),
+                o_cc => tuile_outputs(i)
+            );
+    end generate gen_tuile_instances;
+    process(i_tile_id, tuile_outputs)
+        variable idx : integer;
+    begin
+        idx := to_integer(unsigned(i_tile_id));
+        if idx >= 0 and idx <= 7 then
+            o_colorCode <= tuile_outputs(idx);
+        else
+            o_colorCode <= (others => '0');
+        end if;
+    end process;
 
-o_color_code <= "0000";
 end Behavioral;
